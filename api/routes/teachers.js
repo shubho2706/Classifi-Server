34a96df04f4050d4c372963a60f5d679d727fd99
Shubham <shubho.193@gmail.com>
2018-04-23 14:34:01 +0530

    courses.js updated

--- api/routes/teachers.js
+++ api/routes/teachers.js
@@ -21,6 +21,38 @@ router.post('/', (req, res, next) => {
     });
 });
 
+router.post('/register',(req, res, next) =>{
+    teacher = {
+        emailID : req.body.emailID,
+        address : req.body.address,
+        phoneNumber : req.body.phoneNumber,
+        name : req.body.name,
+        dob : req.body.dob,
+        dept : req.body.dept,
+        gender : req.body.gender,
+        lastUpdated : req.body.lastUpdated,
+        passwordHash : req.body.passwordHash,
+        account : req.body.account,
+        collegeName : req.body.collegeName,
+        yearOfJoining : req.body.yearOfJoining,
+        jobDesignation : req.body.jobDesignation,
+        pastJob : req.body.pastJob,
+        maxQualification : req.body.maxQualification
+    };
+
+    res.status(201).json({
+        message : 'Teacher Details updated',
+        name : teacher
+    });
+});
+
+router.get('/register', (req, res, next) => {
+    res.status(200).json({
+        message : 'All okay'
+    });
+});
+
+
 //Taking parameters in GET request
 router.get('/:teacherEmailId', (req, res, next) => {
 
